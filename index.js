6186dd04016048efa5f4caf90b814eea57144944
Stephen Nerby <stephen.a.nerby@gmail.com>
2022-01-09 13:02:36 -0500

    commit tree 432b67597046f81e34b1edb3312a8b990a0037c9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(collection, string) {
+    return collection.filter(
+        (collection) => collection.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(collection, string) {
+    const result = drivers.filter(drivers => drivers.startsWith(string))
+        return result
+}
+
+const driverDirectory = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ]
+
+function matchName(collection, string) {
+    const result = driverDirectory.filter(collection => collection.name === string)
+    return result
+}
